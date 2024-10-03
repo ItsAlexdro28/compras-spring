669d3b393bdfeae1ce9ff84dfb4448239507836e
itsalexdro28 <alejandroj5025@gmail.com>
2024-10-03 12:53:50 -0500

    fix: :bug: bugfixes

--- src/main/java/com/miscompras/infrastructure/controllers/ProductController.java
+++ src/main/java/com/miscompras/infrastructure/controllers/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.miscompras.application.services.ProductService;
 import com.miscompras.domain.entity.Product;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
